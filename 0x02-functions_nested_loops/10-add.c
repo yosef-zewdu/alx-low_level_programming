0c321691c2d47d39b7693bbe008504faf2795626
Yosef Zewdu <yosephzewdu10@gmail.com>
2022-03-15 23:30:55 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - print lowercase letters 1
+ * @n : character to be added
+ * @m : character to added
+ * Return: result
+ */
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+	return (result);
+}
