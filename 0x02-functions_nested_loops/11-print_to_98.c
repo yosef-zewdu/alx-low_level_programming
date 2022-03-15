6cd4953b94eacbe1211cc184fce814e8c51a0f7a
Yosef Zewdu <yosephzewdu10@gmail.com>
2022-03-16 00:31:18 +0300

    changing for loop to k--

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,12 +24,12 @@ void print_to_98(int n)
 	else if (n >= 98)
 	{
 
-		for (k = 98; k <= n; k++)
+		for (k = n; k >= 98; k--)
 		{
-			if (k != n)
+			if (k != 98)
 				printf("%d, ", k);
 
-			else if (k == n)
+			else if (k == 98)
 				printf("%d\n", k);
 		}
 	}
